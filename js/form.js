1a2001bf701fc9b316f87d09cde8322c9b3b222f
kauane <kauane.saturnino.franca@escola.pr.gov.br>
2025-10-23 16:23:47 -0300

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -1,26 +1,29 @@
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
-    
+
     var form = document.querySelector("#form-adiciona");
+
     var paciente = obtemPacienteDoFormulario(form);
-    
+
     var pacienteTr = montaTr(paciente);
 
-    var erro = validaPaciente(paciente);
+    var erros = validaPaciente(paciente);
 
-    if (erro.length > 0) {
-        var mensagensErro = document.querySelector("#mensagem-erro");
-        mensagensErro.textContent = erro;
+    if (erros.length > 0) {
+        exibeMensagensDeErro(erros);
+
         return;
     }
 
-    //adicionando o paciente na tabela
     var tabela = document.querySelector("#tabela-pacientes");
 
     tabela.appendChild(pacienteTr);
 
     form.reset();
+
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
 
 });
 
@@ -61,9 +64,41 @@ function montaTd(dado, classe) {
 function validaPaciente(paciente) {
 
     var erros = [];
-    if (erros.length > 0) {
-        exibeMensagensDeErro(erros);
 
-        return;
+    if (paciente.nome.length == 0) {
+        erros.push("O nome não pode ser em branco");
     }
+
+    if (paciente.gordura.length == 0) {
+        erros.push("A gordura não pode ser em branco");
+    }
+
+    if (paciente.peso.length == 0) {
+        erros.push("O peso não pode ser em branco");
+    }
+
+    if (paciente.altura.length == 0) {
+        erros.push("A altura não pode ser em branco");
+    }
+
+    if (!validaPeso(paciente.peso)) {
+        erros.push("Peso é inválido");
+    }
+
+    if (!validaAltura(paciente.altura)) {
+        erros.push("Altura é inválida");
+    }
+
+    return erros;
+}
+
+function exibeMensagensDeErro(erros) {
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
 }
